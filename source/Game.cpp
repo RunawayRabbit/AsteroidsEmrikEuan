d221d8d14cb27f439ad88fa80dd15292385cc404
ejt <RunawayRabbitGames@gmail.com>
2021-02-23 06:51:39 +0100

    Spritesheet Prototype
    God this looks awful

--- source/Game.cpp
+++ source/Game.cpp
@@ -2,7 +2,9 @@
 #include <SDL.h>
 #include <iostream> // for error messages
 
-#include <SDL_image.h> // Move out into Renderer
+// Move out into Renderer
+#include <SDL_image.h> 
+#include <cmath> // for fmod
 
 #include "Game.h"
 
@@ -23,7 +25,7 @@ Game::Game(std::string windowName, int width, int height)
 	if (!renderer)
 		ExitWithSDLError("Error getting renderer from window");
 	
-	SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
 	
 	if (!(IMG_Init(IMG_INIT_PNG) & IMG_INIT_PNG))
 	{
@@ -82,9 +84,51 @@ void Game::Update()
 	//stub
 }
 
+float AngleDelta(float a, float b)
+{
+	const float TAU{ 6.28318530717958647692 };
+	float delta = fmod((a - b), TAU);
+	float mapped = fmod(delta* 2.0f, TAU) - delta;
+	return mapped;
+}
+
 void Game::Render()
 {
+	// All of this goes out to the renderer.
 	SDL_RenderClear(renderer);
+
+	static float t = 0;
+	t += 2.0f;
+
+	SDL_Rect asteroidSrc;
+	asteroidSrc.w = 58;
+	asteroidSrc.h = 61;
+	asteroidSrc.x = 66;
+	asteroidSrc.y = 194;
+
+	SDL_Rect asteroidDest;
+	asteroidDest.w = 58;
+	asteroidDest.h = 61;
+	asteroidDest.x = 500;
+	asteroidDest.y = 300;
+	SDL_RenderCopyEx(renderer, spritesheet, &asteroidSrc, &asteroidDest,
+		t, NULL, SDL_FLIP_NONE);
+
+
+	SDL_Rect shipSrc;
+	shipSrc.w = 22;
+	shipSrc.h = 30;
+	shipSrc.x = 5;
+	shipSrc.y = 1;
+
+	SDL_Rect shipDest;
+	shipDest.w = 22;
+	shipDest.h = 30;
+	shipDest.x = 400;
+	shipDest.y = 300;
+
+	SDL_RenderCopyEx(renderer, spritesheet, &shipSrc, &shipDest, -t/2,
+		NULL, SDL_FLIP_NONE);
 
 	SDL_RenderPresent(renderer);
 }
